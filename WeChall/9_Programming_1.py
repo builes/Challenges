1e534936af2acd114a7984d7cd42c64b5577a0c9
Ricardo <builes@users.noreply.github.com>
2018-06-10 23:54:02 -0500

    Update 9_Programming_1.py

--- WeChall/9_Programming_1.py
+++ WeChall/9_Programming_1.py
@@ -1,9 +1,9 @@
 import requests;
 
 url_message = 'http://www.wechall.net/challenge/training/programming1/index.php?action=request';
-cookie  	= {'WC': '10420561-22898-3SO7ObNdoApqZGmj'};
-r 			= requests.get(url_message,cookies=cookie);
+cookie  	  = {'WC': '10420561-22898-3SO7ObNdoApqZGmj'};
+r 			    = requests.get(url_message,cookies=cookie);
 o           = r.text;
 url_destine = 'http://www.wechall.net/challenge/training/programming1/index.php?answer='+o;
-d  			= requests.get(url_destine,cookies=cookie);
+d  			    = requests.get(url_destine,cookies=cookie);
 print 		d.text;
