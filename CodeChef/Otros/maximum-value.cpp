948c1657780d024506168bfa4ed70fe50ebb043d
Ricardo <builes@users.noreply.github.com>
2019-08-04 15:01:32 -0500

    Update maximum-value.cpp

--- CodeChef/Otros/maximum-value.cpp
+++ CodeChef/Otros/maximum-value.cpp
@@ -7,40 +7,42 @@ using namespace std;
 
 int main(){
   
-  
-  
   int t;
   cin >> t;
   cin.get();
+  
   int k = 0;
   
-while(k < t){
+    while(k < t){
 
-	vector<int> vec;
-  	string line;
-	int mayor = -1;
+	    vector<int> vec;
+  	    string line;
+	    int mayor = -1;
   	
-  	getline(cin, line);
-  	stringstream ss(line);
-    int num;
-    while(ss>>num){
-        vec.push_back(num);
-    }
-  	for(int i = 0; i < vec.size();i++){
-  		if(vec[i] == vec.size()-1){
-  			vec.erase(vec.begin() + i);
-  			break;
-		}
-	}
-	for(int j = 0; j < vec.size();j++){
-  		if(vec[j] > mayor){
-  			mayor = vec[j];
-		}
-	}
-	
-  	cout << mayor << endl;
-  	
-  	k++;
+  	    getline(cin, line);
+  	    stringstream ss(line);
+        int num;
+        
+        while(ss>>num){
+            vec.push_back(num);
+        }
+  	    
+  	    for(int i = 0; i < vec.size();i++){
+  	    	if(vec[i] == vec.size()-1){
+  	    		vec.erase(vec.begin() + i);
+  	    		break;
+	    	}
+	    }
+	    
+	    for(int j = 0; j < vec.size();j++){
+  	    	if(vec[j] > mayor){
+  	    		mayor = vec[j];
+	    	}
+	    }
+	    
+  	    cout << mayor << endl;
+  	    
+  	    k++;
 }
 
 
