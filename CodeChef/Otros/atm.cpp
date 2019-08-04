294447f0c2e76f3e63a6fe4255806582488c515f
Ricardo <builes@users.noreply.github.com>
2019-08-04 14:57:08 -0500

    Update atm.cpp

--- CodeChef/Otros/atm.cpp
+++ CodeChef/Otros/atm.cpp
@@ -8,25 +8,19 @@ using namespace std;
 
 int main(){
 
+    //Read()
 	int x;
 	double y;
 	cin >> x >> y;
 	
+	//Code()
 	if(y > 0 && y - x - 0.50 >= 0){
-		
 		if(x % 5 == 0){
-			
 			y = y - x - 0.50;
-			
 		}
-		
 	}
 	
 	cout<<fixed<<setprecision(2)<<y;
-
-
-
-
 
 return 0;
 
